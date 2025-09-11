14aba56a86b5157c077122a3cb32cc2afe88b0b6
alaniltaylor-hash <alani.l.taylor@gmail.com>
2025-09-11 11:23:45 -0400

    Add files via upload

---
+++ P1LAB1_LastFirst.py
@@ -0,0 +1,17 @@
+# Alani Taylor
+# 9/11/2025
+# P1LAB1
+# Get input from user and display back to them
+
+
+# Get first and last name from user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print()
+# Display greeting using first_name and last_name variables
+# Use concatenation
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
+print()
+
+# Use a comma
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
